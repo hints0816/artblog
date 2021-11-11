4037235ba439c030b67319dadb2b7158c56629db
hints0816 <1152536969@qq.com>
2021-11-12 00:14:14 +0800

    master

---
+++ blog/src/pages/home/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <q-page padding class="flex justify-center">
+    <q-list padding>
+      <q-item clickable v-ripple class="q-mt-lg">
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.
+            Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.
+            Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.
+            Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.
+            Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+          <q-icon name="star" color="yellow" />
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>Voted!</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-badge color="teal" label="10k" />
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>2 min ago</q-item-label>
+          <div class="text-orange">
+            <q-icon name="star" />
+            <q-icon name="star" />
+            <q-icon name="star" />
+          </div>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset />
+
+      <q-item>
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>meta</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<style lang="scss" scoped>
+.active-class:hover{
+    box-shadow: 4px 4px 2px #888
+}
+</style>
---
+++ blog/src/router/routes.ts
@@ -0,0 +1,20 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        path: '',
+        component: () => import('pages/home/index.vue'),
+      }
+    ],
+  },
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/Error404.vue'),
+  }
+];
+
+export default routes;
