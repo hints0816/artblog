eea969ccb56271ab55b2abed202ce83d276f390d
hints0816 <1152536969@qq.com>
2021-11-12 23:56:15 +0800

    master

---
+++ blog/src/components/ItemCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div v-for="post in postList" :key="post.index">
+      <q-item clickable v-ripple class="q-mt-lg">
+        <q-item-section @click="toPostDetail(post.number)">
+          <q-item-label>
+            <div class="text-h6 text-cyan-9">{{ post.title }}</div>
+          </q-item-label>
+          <q-item-label lines="4" caption class="text-body1 text-gray-light text-justify">
+            {{ post.body_html }}
+          </q-item-label>
+          
+        </q-item-section>
+        <q-item-section side top>
+            <q-item-label caption>
+                {{ post.created_at }}
+            </q-item-label>
+          <q-chip
+            v-for="label in post.labels"
+            clickable
+            class="label"
+            :color="label.color" 
+            :text-color="label.textcolor" 
+            :icon="label.icon"
+            :key="label.index"
+            @click="chipClickHandler(label.name)"
+          >
+            {{ label.name }}
+          </q-chip>
+        </q-item-section>
+      </q-item>
+        <!-- <q-card-actions align="left">
+            <q-btn flat color="red" icon="favorite" />
+            <q-btn flat color="teal" icon="comment" />
+        </q-card-actions> -->
+      <q-separator spaced inset />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { getCurrentInstance } from 'vue'
+
+export default {
+  name: 'Item',
+  props: {
+    postList: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup () {
+    const {ctx} = getCurrentInstance() as any
+    const method = {
+      toPostDetail() {
+        console.log(ctx)
+      }
+    }
+    return {
+      ...method
+    }
+  },
+  methods: {
+  
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.markdown-body hr{
+    height: unset
+}
+</style>
--- blog/src/pages/home/index.vue
+++ blog/src/pages/home/index.vue
@@ -2,58 +2,46 @@
   <div class="row justify-center">
     <q-page padding class="col-xs-12 col-sm-10 col-md-8">
       <q-list padding>
-        <q-item clickable v-ripple class="q-mt-lg">
-          <q-item-section>
-            <q-item-label>Single line item</q-item-label>
-            <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.
-              Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.
-              Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.
-              Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.
-              Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.
-            </q-item-label>
-          </q-item-section>
-
-          <q-item-section side top>
-            <q-item-label caption>5 min ago</q-item-label>
-            <q-icon name="star" color="yellow" />
-            <q-chip clickable @click="onClick" color="primary" text-color="white" icon="event">
-              Add to calendar
-            </q-chip>
-            <q-chip clickable @click="onClick" icon="bookmark">
-              Bookmark
-            </q-chip>
-            <q-chip clickable @click="onClick" color="teal" text-color="white" icon="bookmark">
-              Bookmark
-            </q-chip>
-            <q-chip clickable @click="onClick" color="red" text-color="white" icon="alarm" label="Set alarm" />
-            <q-chip clickable @click="onClick" color="orange" text-color="white" icon="directions">
-              Get directions
-            </q-chip>
-          </q-item-section>
-        </q-item>
-
-        <q-separator spaced inset />
-
-        <q-item>
-          <q-item-section>
-            <q-item-label>Single line item</q-item-label>
-            <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
-          </q-item-section>
-
-          <q-item-section side top>
-            <q-item-label caption>Voted!</q-item-label>
-          </q-item-section>
-        </q-item>
-
-        <q-separator spaced inset />
-
+        <Item :postList="postList" />
       </q-list>
     </q-page>
   </div>
 </template>
+<script>
+import Item from '../../components/ItemCard.vue';
+export default {
+  name: 'List',
+  components: { Item },
+  data() {
+    return {
+      postList: [
+        {
+          'number':1,
+          'title':'tilte',
+          'created_at':'2021-09-02',
+          'body_html':'Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.Secondary line text. Lorem ipsum dolor sit amet, consectet',
+          'labels':[
+            {
+              'icon':'bookmark',
+              'name':'bookmark'
+            },
+            {
+              'icon':'event',
+              'textcolor':'white',
+              'color':'primary',
+              'name':'Add to calendar'
+            }
+          ]
+        }
+      ],
+    };
+  },
+  watch: {
+  },
+  methods: {
+  },
+  created() {
+  },
+};
+</script>
 
-<style lang="scss" scoped>
-.active-class:hover{
-    box-shadow: 4px 4px 2px #888
-}
-</style>
