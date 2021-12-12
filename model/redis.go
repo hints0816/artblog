52dced88df02cb133d9078b7ca76810deaaae79b
hints0816 <1152536969@qq.com>
2021-12-13 01:20:43 +0800

    /

--- model/redis.go
+++ model/redis.go
@@ -13,7 +13,7 @@ func init() {
 		MaxActive:   0,   //连接池最大连接数量,不确定可以用0（0表示自动定义），按需分配
 		IdleTimeout: 300, //连接关闭时间 300秒 （300秒不使用自动关闭）
 		Dial: func() (redis.Conn, error) { //要连接的redis数据库
-			return redis.Dial("tcp", "47.119.167.128:6379", redis.DialPassword("hints0816"))
+			return redis.Dial("tcp", "47.119.111.110:6379", redis.DialPassword("hints0816"))
 		},
 	}
 }
