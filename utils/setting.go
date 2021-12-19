36f08a0f2e42f583b5ec1948db604f8352284866
hints0816 <1152536969@qq.com>
2021-12-20 00:35:56 +0800

    fix:整合github配置

--- utils/setting.go
+++ utils/setting.go
@@ -18,10 +18,14 @@ var (
 	DbPassWord string
 	DbName     string
 
-	AccessKey  string
-	SecretKey  string
-	Bucket     string
-	QiniuSever string
+	AccessKey   string
+	SecretKey   string
+	Bucket      string
+	MinioServer string
+
+	ClientId     string
+	ClientSecret string
+	RedirectUri  string
 )
 
 func init() {
@@ -31,7 +35,8 @@ func init() {
 	}
 	LoadServer(file)
 	LoadData(file)
-	LoadQiniu(file)
+	LoadMinio(file)
+	LoadGithub(file)
 }
 
 func LoadServer(file *ini.File) {
@@ -49,9 +54,15 @@ func LoadData(file *ini.File) {
 	DbName = file.Section("database").Key("DbName").MustString("ginblog")
 }
 
-func LoadQiniu(file *ini.File) {
-	AccessKey = file.Section("qiniu").Key("AccessKey").String()
-	SecretKey = file.Section("qiniu").Key("SecretKey").String()
-	Bucket = file.Section("qiniu").Key("Bucket").String()
-	QiniuSever = file.Section("qiniu").Key("QiniuSever").String()
+func LoadMinio(file *ini.File) {
+	AccessKey = file.Section("minio").Key("AccessKey").String()
+	SecretKey = file.Section("minio").Key("SecretKey").String()
+	Bucket = file.Section("minio").Key("Bucket").String()
+	MinioServer = file.Section("minio").Key("MinioServer").String()
+}
+
+func LoadGithub(file *ini.File) {
+	ClientId = file.Section("github").Key("ClientId").String()
+	ClientSecret = file.Section("github").Key("ClientSecret").String()
+	RedirectUri = file.Section("github").Key("RedirectUri").String()
 }
